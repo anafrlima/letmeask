25116cc719a4c4cf57e0011e07a566ffe46c0b84
Ana Lima <af.rl@hotmail.com>
2022-04-06 19:00:40 -0300

    Add: new components Home and NewRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
-  return <h1>Let me ask</h1>;
+  return (
+    <>
+      <Home />
+      <NewRoom />
+    </>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./services/firebase";
+
+import "./styles/global.scss";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
